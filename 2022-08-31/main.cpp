7d846bc65c7b86b8014a72e75c38600c4127a873
Yihsiang Liow <yliow@ccis.edu>
2022-08-31 14:29:35 -0400

    no msg

---
+++ 2022-08-31/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void draw_line(int n, char even_char='*', char odd_char='@');
+
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    draw_line(n);
+    draw_line(n, '@', '#');
+    
+    return 0;
+}
+
+void draw_line(int n, char even_char, char odd_char)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << (i % 2 == 0 ? even_char : odd_char); 
+    }
+    std::cout << '\n';
+    return;
+}
