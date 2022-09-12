cffbffa1a72e3165c8815aa72982d74f2c8b1ec6
Yihsiang Liow <yliow@ccis.edu>
2022-09-12 13:27:15 -0400

    no msg

---
+++ 2022-09-12/main.cpp
@@ -0,0 +1,28 @@
+// main.cpp
+#include <iostream>
+#include "Matrix.h" // mat, Mat, mat4x4
+
+int main()
+{
+    Matrix m(3, 4); // m.p_ is double *
+
+    std::cout << "m.nrows(): " << m.nrows() << '\n';
+    std::cout << "m.ncols(): " << m.ncols() << '\n';
+
+    // m(1,2) --> m.operator()(1, 2)
+    std::cout << "m(1, 2): " << m(1, 2) << '\n';
+    m(1, 2) = 42;
+    std::cout << "m(1, 2): " << m(1, 2) << '\n';
+    std::cout << m << '\n';
+
+    Matrix n(3, 4);
+    n(1, 2) = 1;
+    n(2, 2) = 10;
+    
+    m += n;
+    std::cout << m << '\n';
+    
+    return 0;
+
+    // here ... m.~Matrix() is called
+}
